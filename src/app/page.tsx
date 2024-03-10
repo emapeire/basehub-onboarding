6685b4e8f4f0abac8d5316f714186478c9a5fac2
Emanuel Peire <emapeire@outlook.com>
2024-03-10 10:41:32 -0300

    Add gettingStarted query to Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,44 @@ import { basehub } from 'basehub'
 
 const Page = async () => {
   const data = await basehub({ next: { revalidate: 30 } }).query({
-    __typename: true
+    gettingStarted: {
+      _id: true,
+      _slug: true,
+      _title: true,
+      aReferenceToAnotherBlock: {
+        _id: true,
+        _slug: true,
+        _title: true,
+        explanation: true
+      },
+      anImageBlock: {
+        alt: true,
+        aspectRatio: true,
+        fileName: true,
+        fileSize: true,
+        height: true,
+        lastModified: true,
+        mimeType: true,
+        rawUrl: true
+      },
+      baseComponent: {
+        _id: true,
+        _slug: true,
+        _title: true,
+        explanation: true
+      },
+      connect: {
+        aspectRatio: true,
+        fileName: true,
+        fileSize: true,
+        height: true,
+        lastModified: true,
+        mimeType: true,
+        url: true,
+        width: true
+      },
+      description: true
+    }
   })
 
   return <pre>{JSON.stringify(data, null, 2)}</pre>
